877a179f9935097866690e1ae2a7ccacca05ec64
Weronika <weronica97@gmail.com>
2018-12-08 00:50:30 +0100

    Generate all entities from db

---
+++ cookbook/src/main/java/cookbook/controllers/UserController.java
@@ -0,0 +1,26 @@
+package cookbook.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import cookbook.database.UserRepository;
+import cookbook.payloads.UserIdentityAvailability;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	
+	@GetMapping("/user/checkUsernameAvailability")
+	public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+		Boolean isAvailable = !userRepository.existsByUsername(username);
+		return new UserIdentityAvailability(isAvailable);
+	}
+
+}
---
+++ cookbook/src/main/java/cookbook/payloads/UserProfile.java
@@ -0,0 +1,5 @@
+package cookbook.payloads;
+
+public class UserProfile {
+
+}
