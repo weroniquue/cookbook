917daa3692b97a5a85bce0c21e3a72413f8dea20
W1ncenty <okonek.wojciech@gmail.com>
2019-01-09 10:06:37 +0100

    new acc commit

--- front-CookBook/README.md
+++ front-CookBook/README.md
@@ -1,6 +1,6 @@
-# FrontCookBook
+# FrontCookbook
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.0.
 
 ## Development server
 
--- front-CookBook/src/app/app.component.spec.ts
+++ front-CookBook/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-CookBook'`, () => {
+  it(`should have as title 'front-Cookbook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('front-CookBook');
+    expect(app.title).toEqual('front-Cookbook');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to front-CookBook!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to front-Cookbook!');
   });
 });
