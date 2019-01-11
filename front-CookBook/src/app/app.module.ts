77d8961e3dbee0dd58ab7a01d32cb7813e920086
W1ncenty <okonek.wojciech@gmail.com>
2019-01-11 18:35:19 +0100

    add login component css

--- front-CookBook/src/app/app.module.ts
+++ front-CookBook/src/app/app.module.ts
@@ -6,16 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecipeComponent } from './recipe/recipe.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
