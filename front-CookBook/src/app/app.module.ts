ea4bb6ccfb901a43a59f746faffe5ce87223f26a
W1ncenty <okonek.wojciech@gmail.com>
2019-01-11 17:41:00 +0100

    add components

--- front-CookBook/src/app/app.module.ts
+++ front-CookBook/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeComponent } from './recipe/recipe.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
