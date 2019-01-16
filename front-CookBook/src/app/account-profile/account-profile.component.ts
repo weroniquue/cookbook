cbd915dcb450c51c492bc0005974b487acdf907c
W1ncenty <okonek.wojciech@gmail.com>
2019-01-16 20:23:56 +0100

    user profiles

--- front-CookBook/src/app/account-profile/account-profile.component.ts
+++ front-CookBook/src/app/account-profile/account-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserProfileData } from '../models/user-profile-data';
+import { UserService } from '../user.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-account-profile',
@@ -8,10 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AccountProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  userProfileData: UserProfileData;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
     const username = this.route.snapshot.paramMap.get('username');
+    this.userService.getUserDetails(username)
+      .subscribe((data: UserProfileData) => this.userProfileData = { ...data },
+      err => {
+        console.log(err);
+      });
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
